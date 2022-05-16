bef24d7ad4e9bf68f1e4333e890e741df9fde29c
dongsun1 <le12352@naver.com>
2022-05-16 15:07:18 +0900

    완강

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,43 @@
+import { UpdateMovieDto } from './dto/update-movie.dto';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly MoviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.MoviesService.getAll();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') movieId: number): Movie {
+    return this.MoviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.MoviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movieId: number) {
+    return this.MoviesService.deleteOne(movieId);
+  }
+
+  @Patch('/:id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDto) {
+    return this.MoviesService.update(movieId, updateData);
+  }
+}
